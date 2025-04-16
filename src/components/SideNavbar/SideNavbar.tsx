f67583fd5f3d640a91c80f1659662d89925afeec
Özcan Sert <ozcsert0@gmail.com>
2025-04-16 17:23:08 +0300

    [AP-7] - added: button for sideNavbar toggle under md screens

--- src/components/SideNavbar/SideNavbar.tsx
+++ src/components/SideNavbar/SideNavbar.tsx
@@ -1,8 +1,10 @@
-import { Navigation, Frame } from "@shopify/polaris";
+import { Navigation, Frame, Icon } from "@shopify/polaris";
 import { HomeIcon, OrderIcon } from "@shopify/polaris-icons";
 import { useNavigate, useLocation } from "react-router-dom";
 import logoSpaceCowboy from "@/assets/logo-spaceCowboy.svg";
 import style from "./SideNavbar.module.scss";
+import { MenuIcon } from "@shopify/polaris-icons";
+
 interface NavigationItem {
   url: string;
   label: string;
@@ -38,23 +40,35 @@ const SideNavbar: React.FC = () => {
     onClick: () => handleNavigate(item.url),
   }));
 
+  const handleMenuToggle = () => {
+    console.log("Menu toggled");
+  };
+
   return (
-    <aside className={style.sideNavbarContainer}>
-      <Frame>
-        <Navigation location={location.pathname}>
-          <div className={style.navigationContainer}>
-            <div className={style.logoWrapper}>
-              <img
-                src={logoSpaceCowboy}
-                alt="Company Logo"
-                className={style.logo}
-              />
+    <>
+      <button
+        className={style.hamburgerMenu}
+        onClick={() => handleMenuToggle()}
+      >
+        <Icon accessibilityLabel="hamburgerMenuToggle" source={MenuIcon} />
+      </button>
+      <aside className={style.sideNavbarContainer}>
+        <Frame>
+          <Navigation location={location.pathname}>
+            <div className={style.navigationContainer}>
+              <div className={style.logoWrapper}>
+                <img
+                  src={logoSpaceCowboy}
+                  alt="Company Logo"
+                  className={style.logo}
+                />
+              </div>
+              <Navigation.Section items={items} />
             </div>
-            <Navigation.Section items={items} />
-          </div>
-        </Navigation>
-      </Frame>
-    </aside>
+          </Navigation>
+        </Frame>
+      </aside>
+    </>
   );
 };
 
